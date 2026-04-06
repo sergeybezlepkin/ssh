2c0b86f9aa25311386d528c2480c0e824522240f
Sergey Bezlepkin <mrider828@gmail.com>
2026-04-06 22:25:53 +0300

    Complete finished file

--- ssh.sh
+++ ssh.sh
@@ -4,15 +4,15 @@ if [ ! -d "$HOME/.ssh" ]; then
     mkdir -p "$HOME/.ssh"
     chmod -R 700 "$HOME/.ssh"
 fi
-	
+
 sleep 1
-	
+
 if [ -f /etc/os-release ]; then
 	if [ ! -f "$HOME/.ssh/authorized_keys" ]; then
 		touch "$HOME/.ssh/authorized_keys"
 	fi
 fi
-	
+
 sleep 1
 echo
 
@@ -28,10 +28,10 @@ if [ ! -f "$HOME/.ssh/id_rsa" ]; then
     else
         echo "SSH key already exists"
 fi
-	
+
 sleep 1
 echo
-	
+
 while true; do
     read -p "Enter IPv4 address for node: " ip
         if [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
@@ -81,18 +81,6 @@ fi
 sleep 1
 echo
 
-read -p "Disable password authentication for the node $ip? (y/n) " yn
-	if [[ "${yn}" =~ ^(y|yes)$ ]]; then
-		echo "Connecting to $ip as $user to apply changes"
-		ssh "$user@$ip" "sudo -S sed -i '/^#*PasswordAuthentication/d' /etc/ssh/sshd_config && echo 'PasswordAuthentication no' | sudo -S tee -a /etc/ssh/sshd_config && sudo systemctl restart sshd"
-	else
-		echo "Skipping disabling password authentication for the node $ip"
-	fi
-
-sleep 1
-echo
-<<<<<<< HEAD
-	
 read -p "Change the standard port (22) to the specified one? (y/n) " yn
 	if [[ "${yn}" =~ ^(y|yes)$ ]]; then
 		read -p "Enter password for $user@$ip: " paswd
@@ -102,16 +90,16 @@ read -p "Change the standard port (22) to the specified one? (y/n) " yn
 		if [[ "${port}" =~ ^[0-9]+$ ]] && [ "$port" -ge 49152 ] && [ "$port" -le 65535 ]; then
 			echo "Attempting to change port to $port"
 			ssh "$user@$ip" "PASWD=\"$paswd\" PORT=\"$port\" bash -s" << 'EOF'
-			
+
 			PASWD="$PASWD"
 			PORT="$PORT"
-			
+
 			do_sudo() {
                     	echo "$PASWD" | sudo -S bash -c "$1"
 			}
-                
+
 			do_sudo "sed -i '/^#\?Port/d' /etc/ssh/sshd_config && echo 'Port $PORT' >>/etc/ssh/sshd_config"
-			
+
 			if systemctl is-active --quiet firewalld; then
 				do_sudo "firewall-cmd --permanent --remove-service=ssh 2>/dev/null && firewall-cmd --permanent --add-port=$PORT/tcp && firewall-cmd --reload"
 			else
@@ -119,7 +107,7 @@ read -p "Change the standard port (22) to the specified one? (y/n) " yn
 					do_sudo "iptables -D INPUT -p tcp --dport 22 -j ACCEPT 2>/dev/null && iptables -A INPUT -p tcp --dport $PORT -j ACCEPT"
 				fi
 			fi
-			
+
 			do_sudo "systemctl restart sshd"
 EOF
 			break
@@ -138,13 +126,24 @@ unset paswd
 sleep 1
 echo
 
+read -p "Disable password authentication for the node $ip? (y/n) " yn
+        if [[ "${yn}" =~ ^(y|yes)$ ]]; then
+                echo "Connecting to $ip as $user to apply changes"
+                ssh "$user@$ip" "sudo -S sed -i '/^#*PasswordAuthentication/d' /etc/ssh/sshd_config && echo 'PasswordAuthentication no' | sudo -S tee -a /etc/ssh/sshd_config && sudo systemctl restart sshd"
+        else
+                echo "Skipping disabling password authentication for the node $ip"
+        fi
+
+sleep 1
+echo
+
 read -p "Do you want to create a configuration file for a quick connection to the server? For example, enter ssh my-server in the terminal (y/n) " yn
 if [[ "${yn}" =~ ^(y|yes)$ ]]; then
 	if [ ! -f "$HOME/.ssh/config" ]; then
 		touch "$HOME/.ssh/config"
 		chmod 600 "$HOME/.ssh/config"
 	fi
-	
+
 	while true; do
 	read -p "Enter a short name for the connection (my-server) " nameserver
 		if [[ "${nameserver}" =~ ^[a-zA-Z0-9-]+$ ]]; then
@@ -170,7 +169,3 @@ fi
 
 echo
 echo "SSH setup completed"
-=======
-
-echo "SSH setup completed"
->>>>>>> 31ff1448a8ecb0829adea389419675ecf869eaf2
